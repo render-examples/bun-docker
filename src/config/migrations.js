fa1d2b34120800228a1d400f5f74d1b4aee94087
Yoda <sebastian.m.e89@gmail.com>
2024-05-13 00:38:14 -0400

    docker migration package

--- src/config/migrations.js
+++ src/config/migrations.js
@@ -2,7 +2,12 @@ import { migrate } from 'drizzle-orm/mysql2/migrator';
 import { db, client } from '../config/db';
 
 // This will run migrations on the database, skipping the ones already applied
-await migrate(db, { migrationsFolder: './drizzle' });
+await migrate(db, { migrationsFolder: './drizzle' }).catch((err) => {
+    console.error(err);
+  })
+  .then(() => {
+    console.log("Migrations ran successfully");
+  });
 
 // Don't forget to close the connection, otherwise the script will hang
 await client.end();
