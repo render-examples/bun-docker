0a7272aae970cae82595eea2a5c8df9071b009bf
Yoda <sebastian.m.e89@gmail.com>
2024-05-31 15:51:47 -0400

    default route

--- src/index.js
+++ src/index.js
@@ -9,7 +9,9 @@ import { admin_routes } from './routes/v1/admin/login'
 const app = new Hono()
 
 app.use('/logo.png',serveStatic({path: './static/logo.png'}))
-
+app.get('/', c => {
+    return c.redirect('/admin/login')
+})
 app.route('/api/v1', v1)
 app.route('/admin', admin_routes)
 
