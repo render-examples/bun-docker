42706b796a094d9fde724908050c619e2842d963
Yoda <sebastian.m.e89@gmail.com>
2024-06-01 16:03:09 -0400

    date utc

--- src/routes/v1/utils/generalUtils.js
+++ src/routes/v1/utils/generalUtils.js
@@ -5,14 +5,14 @@ const moment = require('moment');
 
 export const dateStringToTimestamp = (dateString) => {
     
-    let timestamp = moment(dateString).format('YYYY-MM-DD HH:mm:ss');
+    let timestamp = moment.utc(dateString).format('YYYY-MM-DD HH:mm:ss');
     return new Date(timestamp)
 }
 
 export const timeStringToTimestamp = (timeString) => {
  
-    let currentdate = moment().format('YYYY-MM-DD');
+    let currentdate = moment.utc().format('YYYY-MM-DD');
     let datetimeString = `${currentdate} ${timeString}`;
-    let time = moment(datetimeString).format('YYYY-MM-DD HH:mm:ss');
+    let time = moment.utc(datetimeString).format('YYYY-MM-DD HH:mm:ss');
     return new Date(time)
 }
