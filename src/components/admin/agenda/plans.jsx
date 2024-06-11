076c0248d586f3298f8df0f0b99820c90173a0d4
Yoda <sebastian.m.e89@gmail.com>
2024-06-11 00:46:24 -0400

    plans fix

--- src/components/admin/agenda/plans.jsx
+++ src/components/admin/agenda/plans.jsx
@@ -1,26 +1,38 @@
 import { html} from 'hono/html'
 
 const SelectPlans = (data) => {
-    return(html
-        `
-        <label class="label">Plan</label>
+    return(
+        <div>
+            <label class="label">Plan</label>
         <div class="control">
             <div class="select" >
                 <select id="planId" hx-get="/api/v1/planes" hx-swap="none" hx-trigger="load" name="planId">
                 </select>
             </div>
         </div>
-        <script>
-            document.getElementById('planId').addEventListener('htmx:afterRequest', evt => {
-                let select = document.getElementById('planId')
-                let plans = evt.detail.xhr.response
-                plans = JSON.parse(plans)
-                select.innerHTML = plans.map(plan => {
-                    return \`<option value="\${plan.id}" \${plan.id === ${data.data} ? 'selected' : ''}>\${plan.name}</option>\`
-                }).join('')
-            })
-        </script>
-        `
+
+        {(data.data) ? html`
+            <script>
+                document.getElementById('planId').addEventListener('htmx:afterRequest', evt => {
+                    let select = document.getElementById('planId')
+                    let plans = evt.detail.xhr.response
+                    plans = JSON.parse(plans)
+                    select.innerHTML = plans.map(plan => {
+                        return \`<option value="\${plan.id}" \${plan.id === ${data.data} ? 'selected' : ''}>\${plan.name}</option>\`
+                    }).join('')
+                })
+            </script>` : html`
+            <script>
+                document.getElementById('planId').addEventListener('htmx:afterRequest', evt => {
+                    let select = document.getElementById('planId')
+                    let plans = evt.detail.xhr.response
+                    plans = JSON.parse(plans)
+                    select.innerHTML = plans.map(plan => {
+                        return \`<option value="\${plan.id}">\${plan.name}</option>\`
+                    }).join('')
+                })
+            </script>`}
+        </div>
     )
 }
 
